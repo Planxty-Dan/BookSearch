bc6812e095cbecba4a9091e024c07040183bc1af
Planxty-Dan <dan.newport@detroitlabs.com>
2014-10-01 16:53:06 -0400

    added link from textview layout to main and printed Set in Java to the screen

--- app/src/main/java/com/example/admin/omgandroid/MyActivity.java
+++ app/src/main/java/com/example/admin/omgandroid/MyActivity.java
@@ -4,14 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
+
+    TextView mainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java");
     }
 
 
